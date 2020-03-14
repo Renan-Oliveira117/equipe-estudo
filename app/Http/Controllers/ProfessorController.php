d43f35e00d3d337e7c0dbac50cc2200a422508a8
Allmeida <allmeida.fran@gmail.com>
2020-03-14 12:14:32 -0400

    tabela professor

---
+++ app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Professor;
+use Illuminate\Http\Request;
+
+class ProfessorController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Professor $professor)
+    {
+        //
+    }
+
+    public function edit(Professor $professor)
+    {
+        //
+    }
+
+    public function update(Request $request, Professor $professor)
+    {
+        //
+    }
+
+    public function destroy(Professor $professor)
+    {
+        //
+    }
+}
